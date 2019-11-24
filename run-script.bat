e8943f378b918e494ccede5d86153b041d7e9064
Mistralys <s.mordziol@gmail.com>
2019-11-24 17:40:06 +0100

    Added a windows batch file to run the script.

---
+++ run-script.bat
@@ -0,0 +1,15 @@
+@echo off
+
+cls
+
+echo ------------------------------------------------
+echo UPDATING EASYPHP CONFIGURATION
+echo ------------------------------------------------
+echo.
+
+php index.php
+
+echo.
+echo.
+
+pause
